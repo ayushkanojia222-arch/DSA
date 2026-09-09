bf0137801769c009952df9bc9c9fddc194cd418a
ayushkanojia222-arch <ayushkanojia222@gmail.com>
2026-09-09 12:32:25 +0530

    Time: 3 ms (42.17%) | Memory: 71.2 MB (33.29%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n=ratings.size();
-        vector<int>candies(n,1);
+        int n = ratings.size();
+        vector<int> candies(n, 1);
 
+        int total = n;
 
-        for(int i =1;i<n;i++)
-        {
-            if(ratings[i]>ratings[i-1])
-            {
-                candies[i]=candies[i-1]+1;
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+                total += candies[i] - 1;
             }
         }
 
-        for(int i =n-2;i>=0;i--)
-        {
-            if(ratings[i]>ratings[i+1])
-            {
-                candies[i]=max(candies[i],candies[i+1] +1);
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                int newCandy = candies[i + 1] + 1;
+
+                if (newCandy > candies[i]) {
+                    total += newCandy - candies[i];
+                    candies[i] = newCandy;
+                }
             }
         }
-        int total =0;
-        for(int c:candies)
-        {
-            total+=c;
-        }
+
         return total;
-
-
-        
     }
 };
